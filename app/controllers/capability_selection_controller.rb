04ed6e9245dfb0997a3251817f35a0b33e6b59f0
Nate Geslin <teamtomkins23@gmail.com>
2024-12-30 11:49:32 -0600

    task/controller-simplification (#21)

--- app/controllers/capability_selection_controller.rb
+++ app/controllers/capability_selection_controller.rb
@@ -6,25 +6,29 @@ class CapabilitySelectionController < ApplicationController
   end
 
   def create
-    success = if params[:guest]
+    if params[:guest]
       @user.make_guest!
-      true
-    else
-      @user.update_capabilities(
-        pilot: params[:pilot] == "1",
-        passenger: params[:passenger] == "1"
-      )
+      redirect_to root_path, notice: 'Capabilities updated successfully.'
+      return
     end
 
-    if success
-      if params[:pilot] == "1" && @user.pilot? && @user.aircraft.none?
-        redirect_to new_aircraft_path, notice: 'Welcome, pilot! Would you like to register your first aircraft? You can always add or modify your aircraft later.'
-      else
-        redirect_to root_path, notice: 'Capabilities updated successfully.'
-      end
-    else
+    @user.update_capabilities(
+      pilot: params[:pilot] == "1",
+      passenger: params[:passenger] == "1"
+    )
+
+    unless @user.errors.empty?
       redirect_to new_capability_selection_path, alert: @user.errors.full_messages.to_sentence
+      return
     end
+
+    if params[:pilot] == "1" && @user.pilot? && @user.aircraft.empty?
+      redirect_to new_aircraft_path, 
+        notice: 'Welcome, pilot! Would you like to register your first aircraft? You can always add or modify your aircraft later.'
+      return
+    end
+
+    redirect_to root_path, notice: 'Capabilities updated successfully.'
   end
 
   private
